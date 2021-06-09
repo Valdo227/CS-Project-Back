5400076036565ffbb5c000cf226b5005d052e60b
Valdo227 <59632809+Valdo227@users.noreply.github.com>
2021-06-09 18:03:13 -0500

    Initial commit

---
+++ src/main/java/com/example/csprojectback/CsProjectBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.csprojectback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsProjectBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsProjectBackApplication.class, args);
+    }
+
+}
